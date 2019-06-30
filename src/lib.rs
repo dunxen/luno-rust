6601d547a5920d3e8979c7a86e8ffe13ac16294b
Duncan Dean <duncan@dev.psiphi.co>
2019-06-30 20:11:32 +0200

    migrate to reqwest for simplicity

--- src/lib.rs
+++ src/lib.rs
@@ -1,179 +1,132 @@
-use std::cell::RefCell;
-use std::io;
-
-use futures::{Future, Stream};
-use hyper::client::HttpConnector;
-use hyper::header::{Authorization, Basic};
-use hyper::{Client, Headers, Uri};
-use hyper_tls::HttpsConnector;
+use reqwest::Client;
+use serde::de::DeserializeOwned;
 use serde::Deserialize;
-use serde_json::Value as JsValue;
-use tokio_core::reactor::Core;
-use url::Url;
 
-struct UriMaker {
-    api_base: String,
+struct UrlMaker {
+    api_base: reqwest::Url,
 }
 
-impl UriMaker {
-    /// Convenience constructor for UriMaker.
-    pub fn new(api_base: String) -> UriMaker {
-        UriMaker { api_base }
-    }
-
-    /// Convert from a `url::Url` to a `hyper::Uri`.
-    fn url_to_uri(url: &url::Url) -> Uri {
-        url.as_str().parse().unwrap()
+impl UrlMaker {
+    /// Convenience constructor for UrlMaker.
+    pub fn new(api_base: String) -> UrlMaker {
+        let url = reqwest::Url::parse(&api_base).unwrap();
+        UrlMaker { api_base: url }
     }
 
     /// Append a path to the API root
-    fn build_url(&self, path: &str) -> Result<Url, url::ParseError> {
-        let url = Url::parse(&self.api_base)?.join(path)?;
-
-        Ok(url)
+    fn build_url(&self, path: &str) -> reqwest::Url {
+        let url = self.api_base.join(path).unwrap();
+        url
     }
-    
+
     /// Build https://api.mybitx.com/api/1/ticker?pair=...
-    pub fn ticker(&self, pair: &str) -> Uri {
-        let mut url = self.build_url("ticker").unwrap();
+    pub fn ticker(&self, pair: &str) -> reqwest::Url {
+        let mut url = self.build_url("ticker");
         url.query_pairs_mut().append_pair("pair", pair);
-        Self::url_to_uri(&url)
+        url
     }
 
     /// Build https://api.mybitx.com/api/1/tickers
-    pub fn tickers(&self) -> Uri {
-        let url = self.build_url("tickers").unwrap();
-        Self::url_to_uri(&url)
+    pub fn tickers(&self) -> reqwest::Url {
+        let url = self.build_url("tickers");
+        url
     }
 
     /// Build https://api.mybitx.com/api/1/orderbook_top?pair=...
-    pub fn orderbook_top(&self, pair: &str) -> Uri {
-        let mut url = self.build_url("orderbook_top").unwrap();
+    pub fn orderbook_top(&self, pair: &str) -> reqwest::Url {
+        let mut url = self.build_url("orderbook_top");
         url.query_pairs_mut().append_pair("pair", pair);
-        Self::url_to_uri(&url)
+        url
     }
 
     /// Build https://api.mybitx.com/api/1/orderbook?pair=...
-    pub fn orderbook(&self, pair: &str) -> Uri {
-        let mut url = self.build_url("orderbook").unwrap();
+    pub fn orderbook(&self, pair: &str) -> reqwest::Url {
+        let mut url = self.build_url("orderbook");
         url.query_pairs_mut().append_pair("pair", pair);
-        Self::url_to_uri(&url)
+        url
     }
 
     /// Build https://api.mybitx.com/api/1/trades?pair=...
-    pub fn trades(&self, pair: &str) -> Uri {
-        let mut url = self.build_url("trades").unwrap();
+    pub fn trades(&self, pair: &str) -> reqwest::Url {
+        let mut url = self.build_url("trades");
         url.query_pairs_mut().append_pair("pair", pair);
-        Self::url_to_uri(&url)
+        url
     }
 }
 
-/// type alias for a custom hyper client, configured for HTTPS
-/// instead of HTTP.
-type HttpsClient = Client<HttpsConnector<HttpConnector>, hyper::Body>;
+struct Credentials {
+    key: String,
+    secret: String,
+}
+
+impl Credentials {
+    fn new(key: String, secret: String) -> Credentials {
+        Credentials { key, secret }
+    }
+}
 
 /// The top level interface for interacting with the remote service.
 pub struct LunoClient {
-    uri_maker: UriMaker,
-    core: RefCell<Core>,
-    http: HttpsClient,
-}
-
-fn to_io_error<E>(err: E) -> io::Error
-where
-    E: Into<Box<std::error::Error + Send + Sync>>,
-{
-    io::Error::new(io::ErrorKind::Other, err)
+    credentials: Credentials,
+    http: Client,
+    url_maker: UrlMaker,
 }
 
 impl LunoClient {
     pub fn new(key: String, secret: String) -> LunoClient {
-        let mut headers = Headers::new();
-        headers.set(Authorization(Basic {
-            username: key,
-            password: Some(secret),
-        }));
-
-        let uri_maker = UriMaker::new("https://api.mybitx.com/api/1/".to_owned());
-
-        let core = Core::new().unwrap();
-
-        let http = {
-            let handle = core.handle();
-            let connector = HttpsConnector::new(4, &handle).unwrap();
-
-            Client::configure().connector(connector).build(&handle)
-        };
+        let credentials = Credentials::new(key, secret);
+        let http = Client::new();
+        let url_maker = UrlMaker::new("https://api.mybitx.com/api/1/".to_owned());
 
         LunoClient {
-            uri_maker,
-            core: RefCell::new(core),
+            credentials,
+            url_maker,
             http,
         }
     }
 
-    fn get_json(&self, uri: hyper::Uri) -> Box<Future<Item = JsValue, Error = io::Error>> {
-        let f = self
-            .http
-            .get(uri)
-            .and_then(|res| {
-                res.body().concat2().and_then(move |body| {
-                    let value: JsValue = serde_json::from_slice(&body).map_err(to_io_error)?;
-                    Ok(value)
-                })
-            })
-            .map_err(to_io_error);
-        Box::new(f)
+    fn get<T>(&self, url: reqwest::Url) -> Result<T, reqwest::Error>
+    where
+        T: DeserializeOwned,
+    {
+        self.http
+            .get(url)
+            .basic_auth(
+                self.credentials.key.to_owned(),
+                Some(self.credentials.secret.to_owned()),
+            )
+            .send()?
+            .json()
     }
 
     /// Get the current ticker for a given trading pair.
-    pub fn get_ticker(&self, pair: &str) -> Result<Ticker, io::Error> {
-        let uri = self.uri_maker.ticker(pair);
-        let work = self.get_json(uri).and_then(|value| {
-            let ticker: Ticker = serde_json::from_value(value).map_err(to_io_error)?;
-            Ok(ticker)
-        });
-        self.core.borrow_mut().run(work)
+    pub fn get_ticker(&self, pair: &str) -> Result<Ticker, reqwest::Error> {
+        let url = self.url_maker.ticker(pair);
+        self.get(url)
     }
 
     /// Get tickers for all available trading pairs.
-    pub fn get_tickers(&self) -> Result<TickerList, io::Error> {
-        let uri = self.uri_maker.tickers();
-        let work = self.get_json(uri).and_then(|value| {
-            let tickers: TickerList = serde_json::from_value(value).map_err(to_io_error)?;
-            Ok(tickers)
-        });
-        self.core.borrow_mut().run(work)
+    pub fn get_tickers(&self) -> Result<TickerList, reqwest::Error> {
+        let url = self.url_maker.tickers();
+        self.get(url)
     }
 
     /// Get a list of the top 100 bids and asks in the order book for a trading pair.
-    pub fn get_orderbook_top(&self, pair: &str) -> Result<Orderbook, io::Error> {
-        let uri = self.uri_maker.orderbook_top(pair);
-        let work = self.get_json(uri).and_then(|value| {
-            let orderbook_top: Orderbook = serde_json::from_value(value).map_err(to_io_error)?;
-            Ok(orderbook_top)
-        });
-        self.core.borrow_mut().run(work)
+    pub fn get_orderbook_top(&self, pair: &str) -> Result<Orderbook, reqwest::Error> {
+        let url = self.url_maker.orderbook_top(pair);
+        self.get(url)
     }
 
     /// Get the full list of bids and asks in the order book for a trading pair.
-    pub fn get_orderbook(&self, pair: &str) -> Result<Orderbook, io::Error> {
-        let uri = self.uri_maker.orderbook(pair);
-        let work = self.get_json(uri).and_then(|value| {
-            let orderbook: Orderbook = serde_json::from_value(value).map_err(to_io_error)?;
-            Ok(orderbook)
-        });
-        self.core.borrow_mut().run(work)
+    pub fn get_orderbook(&self, pair: &str) -> Result<Orderbook, reqwest::Error> {
+        let url = self.url_maker.orderbook(pair);
+        self.get(url)
     }
 
     /// Get the latest trades for a trading pair (limited to 100).
-    pub fn get_trades(&self, pair: &str) -> Result<TradeList, io::Error> {
-        let uri = self.uri_maker.trades(pair);
-        let work = self.get_json(uri).and_then(|value| {
-            let trades: TradeList = serde_json::from_value(value).map_err(to_io_error)?;
-            Ok(trades)
-        });
-        self.core.borrow_mut().run(work)
+    pub fn get_trades(&self, pair: &str) -> Result<TradeList, reqwest::Error> {
+        let url = self.url_maker.trades(pair);
+        self.get(url)
     }
 }
 
