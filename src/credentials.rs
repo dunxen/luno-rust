4e0d5bce3d161332e60e9d58451cec818b8de705
Duncan Dean <duncangleeddean@gmail.com>
2020-03-29 19:44:31 +0200

    Update dependencies & test credentials

--- src/credentials.rs
+++ src/credentials.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub struct Credentials {
     pub key: String,
     pub secret: String,
@@ -6,5 +7,17 @@ pub struct Credentials {
 impl Credentials {
     pub fn new(key: String, secret: String) -> Credentials {
         Credentials { key, secret }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_new() {
+        let (key, secret) = (String::from("TEST_KEY"), String::from("TEST_SECRET"));
+        assert_eq!(
+            super::Credentials::new(key.to_owned(), secret.to_owned()),
+            super::Credentials { key, secret },
+        )
     }
 }
